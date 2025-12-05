8704d98379e3b20669a180baf5ba66d3e527055f
kunalbhat <kunalbhat@gmail.com>
2025-12-04 21:23:39 -0600

    copy teawak

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -55,9 +55,8 @@ export default function Home() {
           </div>
           <div className="col-span-2">
             <p className="mb-4">
-              Experienced enough to lead, hands-on enough to build. A
-              staff-level IC with 18 years spanning design, product, and
-              engineering.
+              Experienced enough to lead, hands-on enough to build. Staff-level
+              IC with 18 years spanning design, product, and engineering.
             </p>
             <p>
               Previously at Magic+Might, Galactic, PayPal, Braintree, Modest,
